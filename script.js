94a260f9ec04c49dc84b09c1b1444534faf1ce57
DuManosso <67271381+DuManosso@users.noreply.github.com>
2024-02-26 11:59:08 -0300

    Aplicando condições e localStorage

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("IMPORTADO")
+const date = new Date().getTime();
+localStorage.getItem('firstTime') == null ? localStorage.setItem('firstTime', 'true') : localStorage.getItem('firstTime');
+localStorage.getItem('showPopup') == null ? localStorage.setItem('showPopup', date.toString()) : localStorage.getItem('showPopup');
+const firstTime = localStorage.getItem('firstTime');
+const showPopup = localStorage.getItem('showPopup');
+
+if(firstTime == 'true' && (date - parseInt(showPopup)) > 2629800000) {
+    document.getElementById('popup').style.display = 'block';
+	document.getElementById('popup-background').style.display = 'block';
+	let botao = document.getElementById('botao');
+	botao.addEventListener("click", function() {
+        document.getElementById('popup').style.display = 'none';
+        document.getElementById('popup-background').style.display = 'none';
+        localStorage.setItem('showPopup', date.toString());
+        localStorage.setItem('firstTime', 'false');
+    })
+} else {
+    if((date - parseInt(showPopup)) > 7889400000){
+        document.getElementById('popup').style.display = 'block';
+        document.getElementById('popup-background').style.display = 'block';
+        let botao = document.getElementById('botao');
+        botao.addEventListener("click", function() {
+        document.getElementById('popup').style.display = 'none';
+        document.getElementById('popup-background').style.display = 'none';
+        localStorage.setItem('showPopup', date.toString());
+        localStorage.setItem('firstTime', 'false');
+    })
+    }
+}
