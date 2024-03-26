a642a4c41062f47899d35bce9ceb4fef472ba5eb
DuManosso <67271381+DuManosso@users.noreply.github.com>
2024-03-26 10:53:41 -0300

    Update script.js
    modificando detalhes

--- script.js
+++ script.js
@@ -6,22 +6,18 @@ const showPopup = localStorage.getItem('showPopup');
 
 if(firstTime == 'true' && (date - parseInt(showPopup)) > 2629800000) {
     document.getElementById('popup').style.display = 'block';
-	document.getElementById('popup-background').style.display = 'block';
 	let botao = document.getElementById('botao');
 	botao.addEventListener("click", function() {
         document.getElementById('popup').style.display = 'none';
-        document.getElementById('popup-background').style.display = 'none';
         localStorage.setItem('showPopup', date.toString());
         localStorage.setItem('firstTime', 'false');
     })
 } else {
     if((date - parseInt(showPopup)) > 7889400000){
         document.getElementById('popup').style.display = 'block';
-        document.getElementById('popup-background').style.display = 'block';
         let botao = document.getElementById('botao');
         botao.addEventListener("click", function() {
         document.getElementById('popup').style.display = 'none';
-        document.getElementById('popup-background').style.display = 'none';
         localStorage.setItem('showPopup', date.toString());
         localStorage.setItem('firstTime', 'false');
     })
