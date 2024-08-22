73c792c05f8ce445dd4ec51156f26c4a0ff5311c
DuManosso <67271381+DuManosso@users.noreply.github.com>
2024-08-22 14:55:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const date = new Date().getTime();
 localStorage.getItem('firstTime') == null ? localStorage.setItem('firstTime', 'true') : localStorage.getItem('firstTime');
 localStorage.getItem('showPopup') == null ? localStorage.setItem('showPopup', date.toString()) : localStorage.getItem('showPopup');
+localStorage.getItem('messagePortalRhLogin') == null ? localStorage.setItem('messagePortalRhLogin', "first") : localStorage.getItem('messagePortalRhLogin');
+
 const firstTime = localStorage.getItem('firstTime');
 const showPopup = localStorage.getItem('showPopup');
+
+const showMessage = localStorage.getItem('messagePortalRhLogin');
+const popupSSO = document.getElementById('pupupSSOLogin');
 
 if(firstTime == 'true' && (date - parseInt(showPopup)) > 2629800000) {
     document.getElementById('popup').style.display = 'flex';
@@ -23,3 +28,21 @@ if(firstTime == 'true' && (date - parseInt(showPopup)) > 2629800000) {
     })
     }
 }
+
+if(showMessage == "first") popupSSO.style.display = "flex";
+
+if((date - parseInt(showMessage)) >= 2629800000) popupSSO.style.display = "flex";
+
+const buttonCloseMessage = document.getElementById('closeMessage');
+const buttonAlreadyUse = document.getElementById('alreadyUse');
+	
+buttonAlreadyUse.addEventListener("click", () => {
+	popupSSO.style.display = "none";
+	localStorage.setItem('messagePortalRhLogin', "notShow");
+})
+
+buttonCloseMessage.addEventListener("click", () => {
+	popupSSO.style.display = "none";
+	localStorage.setItem('messagePortalRhLogin', date.toString());
+})
+
